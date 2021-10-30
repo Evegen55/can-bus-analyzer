ece796a5223f3ba641d8129ac0a376116472e0b2
Evgenii Lartcev <i.dolende@gmail.com>
2021-10-31 02:24:30 +0300

    scans com port by clicking button

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -20,7 +20,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -32,7 +41,7 @@ public class MainController {
     private Stage window;
 
     @FXML
-    public Button startScanCANBusViaComPortButton;
+    public Button startStopScanCANBusViaComPortButton;
     @FXML
     public Button startScanCANDataFileButton;
 
@@ -101,27 +110,124 @@ public class MainController {
         existedComPortsComboBox.getItems().addAll(FXCollections.observableArrayList(comPortsDescriptions));
     }
 
-    public void startScanCANBusViaComPortButtonClicked(final ActionEvent actionEvent) {
-        final int selectedIndexOfComPort = existedComPortsComboBox.getSelectionModel().getSelectedIndex();
+    boolean isComPortScanned = false;
+    //first thread reads from com and write to a buffer
+    //second thread reads from buffer, form can-messages and write into can-messages buffer
+    //third thread reads messages from buffer, validate them and update GUI with new data
+    final ConcurrentLinkedDeque<Integer> charBuffer = new ConcurrentLinkedDeque<>();
+    final ConcurrentLinkedDeque<String> canMessagesBuffer = new ConcurrentLinkedDeque<>();
+    SerialPort comPort;
+
+    final Runnable runnableReaderFromComPort = () -> {
+        final int selectedIndexOfComPort = existedComPortsComboBox
+                .getSelectionModel()
+                .getSelectedIndex();
         if (selectedIndexOfComPort >= 0) {
-            canBusDataTableView.getItems().clear();
-            dataHolders.clear();
-            //TODO 25/11/2020 lartsev: write data here
-            SerialPort comPort = commPorts[selectedIndexOfComPort];
+            comPort = commPorts[selectedIndexOfComPort];
             comPort.openPort();
             comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-            InputStream in = comPort.getInputStream();
-            try {
-                for (int j = 0; j < 1000; ++j){
-                    System.out.print((char) in.read());
+
+            try (InputStream in = comPort.getInputStream()) {
+                while (isComPortScanned) {
+                    int read = in.read();
+                    charBuffer.offer(read);
                 }
-                in.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            comPort.closePort();
+
+        }
+    };
+
+    final Runnable readerCanMessagesFromBuffer = () -> {
+        StringBuilder sb = new StringBuilder();
+        while (isComPortScanned) {
+            if (!charBuffer.isEmpty()) {
+                Integer value = charBuffer.poll();
+                char readFromBuffer = (char) value.intValue();
+                if (readFromBuffer != '\r') {
+                    if (readFromBuffer == '\n') {
+                        canMessagesBuffer.offer(sb.toString());
+                        sb = new StringBuilder();
+                    } else {
+                        sb.append(readFromBuffer);
+                    }
+                }
+            }
+        }
+    };
+
+    final Runnable updateUiFromComPort = () -> {
+        Map<String, DataHolder> stringDataHolderMap = new HashMap<>();
+        while (isComPortScanned) {
+            DataHolder dataHolder;
+            if (!canMessagesBuffer.isEmpty()) {
+                String canMessage = canMessagesBuffer.poll();
+                String[] split = canMessage.split(" ");
+                if (split[0].length() > 2) {
+                    final int lengthOfCanDataByCAN = Integer.parseInt(split[1]);
+                    final int lengthOfCanDataActual = split.length - 2;
+                    final String[] canData;
+                    if (lengthOfCanDataByCAN == lengthOfCanDataActual) {
+                        canData = new String[lengthOfCanDataByCAN]; //0 0 69 0 46 2F BB BB
+                        System.arraycopy(split, 2, canData, 0, lengthOfCanDataByCAN - 1 + 1);
+                        dataHolder = new DataHolder(split[0], lengthOfCanDataByCAN, canData, 1);
+
+                        if (!stringDataHolderMap.containsKey(split[0])) {
+                            stringDataHolderMap.put(split[0], dataHolder);
+                        } else {
+                            DataHolder dataHolderPrev = stringDataHolderMap.get(split[0]);
+                            String[] canDataPrev = dataHolderPrev.getCanData();
+                            for (int i = 0; i < canData.length; i++) {
+                                if (!canDataPrev[i].equals(canData[i])) {
+                                    canDataPrev[i] = "*";
+                                }
+                            }
+                            dataHolderPrev.addCounter();
+                            dataHolderPrev.getUniqueCanMessages().add(dataHolder.getStringCanData());
+                        }
+
+                        ObservableList<DataHolder> dataHolderObservableList = FXCollections.observableArrayList(stringDataHolderMap.values());
+                        canBusDataTableView.getItems().setAll(dataHolderObservableList);
+                    }
+
+                }
+            }
+        }
+    };
+
+    final ExecutorService executorServiceForComPort = Executors.newFixedThreadPool(3);
+    Future<?> submitReaderFromComPort;
+    Future<?> submitReaderCanMessagesFromBuffer;
+    Future<?> submitUpdateUiFromComPort;
+
+    public void startStopScanCANBusViaComPortButtonClicked(final ActionEvent actionEvent) {
+        if (isComPortScanned) {
+            isComPortScanned = false;
+            boolean comPortClosed = comPort == null ? false : comPort.closePort();
+            while (!submitReaderFromComPort.isCancelled() &&
+                   !submitReaderCanMessagesFromBuffer.isCancelled() &&
+                   !submitUpdateUiFromComPort.isCancelled()) {
+                submitReaderFromComPort.cancel(true);
+                submitReaderCanMessagesFromBuffer.cancel(true);
+                submitUpdateUiFromComPort.cancel(true);
+            }
+            startStopScanCANBusViaComPortButton.setText("Start scan CAN bus");
+        } else {
+            isComPortScanned = true;
+            canBusDataTableView.getItems().clear();
+            dataHolders.clear();
+            while ((submitReaderFromComPort == null || submitReaderFromComPort.isDone()) &&
+                   (submitReaderCanMessagesFromBuffer == null || submitReaderCanMessagesFromBuffer.isDone()) &&
+                   (submitUpdateUiFromComPort == null || submitUpdateUiFromComPort.isDone())) {
+                submitReaderFromComPort = executorServiceForComPort.submit(runnableReaderFromComPort);
+                submitReaderCanMessagesFromBuffer = executorServiceForComPort.submit(readerCanMessagesFromBuffer);
+                submitUpdateUiFromComPort = executorServiceForComPort.submit(updateUiFromComPort);
+                startStopScanCANBusViaComPortButton.setText("Stop scan CAN bus");
+            }
         }
     }
+
 
     public void startScanCANDataFileButtonClicked(final ActionEvent actionEvent) {
         final FileChooser fileChooser = new FileChooser();
